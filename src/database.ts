d48f2658f15d1fd75d604cff4b40bceefc07b4f5
Jan Špaček <patek.mail@gmail.com>
2023-04-25 16:03:31 +0200

    Move URL parsing to `@libsql/hrana-client`

--- src/database.ts
+++ src/database.ts
@@ -44,7 +44,7 @@ export class Database extends EventEmitter {
             return new sqlite3.Database(url, mode | OPEN_URI, callback);
         }
 
-        const parsedUrl = parseUrl(url);
+        const parsedUrl = hrana.parseLibsqlUrl(url);
 
         super();
         this.#client = hrana.open(parsedUrl.hranaUrl, parsedUrl.authToken);
@@ -243,52 +243,4 @@ export class Database extends EventEmitter {
     interrupt(): void {
         throw new Error("Database.interrupt() is not implemented");
     }
-}
-
-
-type ParsedUrl = {
-    hranaUrl: string,
-    authToken: string | undefined,
-};
-
-function parseUrl(urlStr: string): ParsedUrl {
-    const url = new URL(urlStr);
-
-    let authToken: string | undefined = undefined;
-    for (const [key, value] of url.searchParams.entries()) {
-        if (key === "authToken") {
-            authToken = value;
-        } else {
-            throw new TypeError(`Unknown URL query argument ${JSON.stringify(key)}`);
-        }
-    }
-
-    let hranaScheme: string;
-    if (url.protocol === "http:") {
-        throw new Error('This client does not support "http:" URLs. Please use a "ws:" URL instead.');
-    } else if (url.protocol === "https:") {
-        throw new Error('This client does not support "https:" URLs. Please use a "wss:" URL instead.');
-    } else if (url.protocol === "libsql:") {
-        hranaScheme = "wss:";
-    } else if (url.protocol === "ws:" || url.protocol === "wss:") {
-        hranaScheme = url.protocol;
-    } else {
-        throw new Error(
-            `This client does not support ${JSON.stringify(url.protocol)} URLs. ` +
-                'Please use a "libsql:", "ws:" or "wss:" URL instead.'
-        );
-    }
-
-    if (url.username || url.password) {
-        throw new TypeError(
-            "This client does not support HTTP Basic authentication with a username and password. " +
-                'You can authenticate using a token passed in the "authToken" URL query parameter.',
-        );
-    }
-    if (url.hash) {
-        throw new TypeError("URL fragments are not supported");
-    }
-
-    const hranaUrl = `${hranaScheme}//${url.host}${url.pathname}`;
-    return { hranaUrl, authToken };
 }
